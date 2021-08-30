61ddda6924b3e19e4ea598d506f7c28c123d72b8
tauhidbs23 <tauhidul.islam@brainstation-23.com>
2021-08-31 00:29:28 +0600

    console.log in react

--- react/src/App.js
+++ react/src/App.js
@@ -2,6 +2,9 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name = 'Tauhidul Islam';
+  console.log('here you go!')
+  console.log(name)
   return (
     <div className="App">
       <header className="App-header">
